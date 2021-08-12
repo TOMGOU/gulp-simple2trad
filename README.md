ec662af20ae063e545187c870261d24362f3b0be
tommy.tang <tommy.tang@lixinchuxing.com>
2021-08-12 15:50:55 +0800

    feat: gulp-simple2trad

---
+++ README.md
@@ -0,0 +1,44 @@
+# gulp-simple2trad
+gulp plugin translate simple to trad
+
+## 安装
+
+```js
+npm install gulp-simple2trad -D
+```
+
+## 使用【gulpfile.js】
+
+```js
+const gulp = require('gulp')
+const cnchar = require('cnchar')
+const trad = require('cnchar-trad')
+const simple2trad = require('gulp-simple2trad')
+const through = require('through2')
+cnchar.use(trad)
+
+/**
+ * 简体字翻译为繁体字
+ */
+const translate = () => {
+  return gulp.src('../dist/lixin/**/*')
+  .pipe(simple2trad())
+  .pipe(through.obj(function (chunk, enc, cb) {
+    cb(null, chunk)
+  }))
+  .pipe(gulp.dest('../dist/test'))
+}
+
+exports.translate = translate
+```
+
+## 参数说明
+> 由于部分简体中文直接翻译为繁体字往往存在特例问题，需要个性化处理某些词汇
+
+```js
+{
+  '餘額': '余額'
+}
+
+// 将【餘額】 替换为 【余額】
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const through = require('through2')
+const PluginError = require('plugin-error')
+const cnchar = require('cnchar')
+const trad = require('cnchar-trad')
+cnchar.use(trad)
+
+module.exports = function(opt) {
+  let options = opt || {}
+
+  options.output = options.output || {}
+
+  function simple2trad(file, encoding, callback) {
+    if (file.isNull()) {
+      this.push(file)
+      return callback()
+    }
+
+    if (file.isStream()) {
+      this.emit('end')
+      return new callback(PluginError('gulp-minify', 'Streaming not supported:' + file.path))
+    }
+
+    let newSource = ''
+    newSource = cnchar.convert.simpleToTrad(file.contents.toString())
+    const optionKeys = Object.keys(options)
+    optionKeys.length && optionKeys.forEach(key => {
+      newSource = newSource.replace(new RegExp(key, 'g'), options[key])
+    })
+    file.contents = Buffer.from(newSource)
+    this.push(file)
+
+    callback()
+  }
+
+  return through.obj(simple2trad)
+}
